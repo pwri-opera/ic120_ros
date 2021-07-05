e17efc031bdf43d5a064fc4f23f946f80e3d9ef4
genkiiii <t.climate.i.snow@gmail.com>
2021-07-05 19:16:33 +0900

    modify service call system

--- scripts/dump_navigation.py
+++ scripts/dump_navigation.py
@@ -10,99 +10,72 @@ import math
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Vector3
-from ic120_nav.srv import dump_nav,dump_navRequest
-
-# X,Y,Theta, pose_flag, wait_for_nav_service_call, wait_for_dumpup_service_call
-waypoints = [
-    [0,0,math.pi,True,False,False],#1
-    [10,0,math.pi,True,False,False],#2
-    [20,0,math.pi,False,True,False],#3
-    [10,0,math.pi,False,True,False],#4
-    [10,-10,-math.pi/2,False,False,True],#5
-    [10,-15,math.pi,True,False,False],#6
-    [20,-15,math.pi,True,False,True],#7
-    [10,-15,math.pi,False,False,False],#8
-    [10,-10,math.pi/2,False,False,False]#9
-    #2に戻る
-]
-
-def euler_to_quaternion(euler):
-    q = tf.transformations.quaternion_from_euler(euler.x, euler.y, euler.z)
-    return Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
-
+from ic120_nav.srv import dump_nav,dump_navRequest,dump_navResponse
 
 def goal_pose(pose):
     print(pose)
     goal_pose = MoveBaseGoal()
-    goal_pose.target_pose.header.frame_id = 'map'
-    goal_pose.target_pose.pose.position.x = pose[0]
-    goal_pose.target_pose.pose.position.y = pose[1]
-    goal_pose.target_pose.pose.position.z = 0.0
-    goal_pose.target_pose.pose.orientation = euler_to_quaternion(Vector3(0.0,0.0,pose[2]))
+    goal_pose.target_pose = pose
     return goal_pose
 
+#service
+rospy.init_node("ic120_navigation")
+client = actionlib.SimpleActionClient('/ic120/move_base', MoveBaseAction) 
+listener = tf.TransformListener()
 
-if __name__ == '__main__':
-    rospy.init_node('patrol')
-    listener = tf.TransformListener()
+def server(req):
+    print("Get service call for navigation")
+    goal = goal_pose(req.target_pose)
+    orientation_flag = req.orientation_flag.data
+    dump_flag = req.dump_flag.data
 
-    print("start")
-
-    client = actionlib.SimpleActionClient('/ic120/move_base', MoveBaseAction) 
-    # while not client.wait_for_server(rospy.Duration(5)):
-    #     rospy.loginfo("Waiting for the move_base action server to come up")
-    client.wait_for_server()
-    listener.waitForTransform("map", "/ic120/base_link", rospy.Time(), rospy.Duration(4.0))
-
-    print("start while loop")
+    client.send_goal(goal)
 
     while True:
-        for pose in waypoints:
-            goal = goal_pose(pose)
-            if(pose[4] == True):
-                print("waiting for service call")
-                rospy.wait_for_service('ic120_nav_srv')
-                while True:
-                    print("waiting for permission from const_manager")
-                    nav_srv_proxy  = rospy.ServiceProxy('ic120_nav_srv', dump_nav)
+        now = rospy.Time.now()
+        listener.waitForTransform("map", "/ic120_tf/base_link", now, rospy.Duration(1.0))
+        position, quaternion = listener.lookupTransform("map", "/ic120_tf/base_link", now)
+
+        if(orientation_flag == True):
+            print("orientation_flat==True")
+            if(client.wait_for_result(rospy.Duration(0.5)) == True):
+                if(dump_flag == True):
+                    print("waiting for dumpup_manager")
+                    rospy.wait_for_service('dumpup_srv') 
+                    dumpup_srv_proxy  = rospy.ServiceProxy('dumpup_srv', dump_nav)
                     request = dump_navRequest()
-                    response = nav_srv_proxy (request)
+                    response = dumpup_srv_proxy (request)
                     rospy.sleep(1)                    
                     if(response.is_ok.data == True):
                         print("permitted")
                         break
-            client.send_goal(goal)
-            pose_flag = pose[3]
-            dumup_flag = pose[5]
-            while True:
-                now = rospy.Time.now()
-                listener.waitForTransform("map", "/ic120/base_link", now, rospy.Duration(4.0))
+                print("next waypoint")
+                break
+            print("moving!")
+        else:
+            print("orientation_flag==False")
+            # ウェイポイントのゴールの周囲１ｍ以内にロボットが来たら、次のウェイポイントを発行する
+            if(math.sqrt((position[0]-goal.target_pose.pose.position.x)**2 + (position[1]-goal.target_pose.pose.position.y)**2 ) <= 1):
+                print("next waitpoint")
+                break
+            else:
+                print("moving!!!")
+                rospy.sleep(0.5)
+    response = dump_navResponse()
+    response.is_ok.data = True
+    return response
 
-                # map座標系の現在位置をtfから取得する
-                position, quaternion = listener.lookupTransform("map", "/ic120/base_link", now)
 
-                if(pose_flag == True):
-                    # print("wait for service back from local/global planner")
-                    if(client.wait_for_result(rospy.Duration(0.5)) == True):
-                        if(dumup_flag == True):
-                            rospy.wait_for_service('dumpup_srv') 
-                            print("waiting for dumpup_manager")
-                            dumpup_srv_proxy  = rospy.ServiceProxy('dumpup_srv', dump_nav)
-                            request = dump_navRequest()
-                            response = dumpup_srv_proxy (request)
-                            rospy.sleep(1)                    
-                            if(response.is_ok.data == True):
-                                print("permitted")
-                                break
-                        print("next waypoint")
-                        break
-                    print("moving")
-                else:
-                    # ウェイポイントのゴールの周囲１ｍ以内にロボットが来たら、次のウェイポイントを発行する
-                    if(math.sqrt((position[0]-goal.target_pose.pose.position.x)**2 + (position[1]-goal.target_pose.pose.position.y)**2 ) <= 1):
-                        print("next waitpoint")
-                        break
-                    else:
-                        print("moving!!")
-                        rospy.sleep(0.5)
-            
+if __name__ == '__main__':
+    #service
+    # rospy.init_node("ic120_navigation")
+    # global client
+    # client = actionlib.SimpleActionClient('/ic120/move_base', MoveBaseAction) 
+    # listener = tf.TransformListener()
+    client.wait_for_server()
+    listener.waitForTransform("map", "/ic120_tf/base_link", rospy.Time(), rospy.Duration(4.0))
+
+    s = rospy.Service("ic120_nav_srv", dump_nav, server)
+    print("Ready to navigation")
+    print("waiting for waypoint from service server")
+    rospy.spin()
